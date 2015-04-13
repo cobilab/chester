9d76053c9afdf611f0f0647e9055782eaef91bd9
pratas <pratas@ua.pt>
2015-04-14 00:27:55 +0100

    FileRefHandlingShell

--- chester.c
+++ chester.c
@@ -258,7 +258,7 @@ void Target(Param *P, uint8_t ref, uint32_t tar){
 // - - - - - - - - - - - - - - - - R E F E R E N C E - - - - - - - - - - - - -
 void LoadReference(Param *P, uint32_t ref){
   FILE     *Reader = Fopen(P->ref->names[ref], "r");
-  uint32_t k, idxPos, header = 0;
+  uint32_t k, idxPos, header = 0, type, line, dna;
   int32_t  idx = 0;
   uint8_t  *rBuf, *sBuf, sym;
   uint64_t i = 0;
@@ -273,20 +273,48 @@ void LoadReference(Param *P, uint32_t ref){
   sBuf  = (uint8_t *) Calloc(BUFFER_SIZE + BGUARD+1, sizeof(uint8_t));
   sBuf += BGUARD;
 
+  sym = fgetc(Reader);
+  switch(sym){
+    case '>': type = 1; break;
+    case '@': type = 2; break;
+    default : type = 0;
+    }
+  rewind(Reader);
+
   while((k = fread(rBuf, 1, BUFFER_SIZE, Reader)))
     for(idxPos = 0 ; idxPos < k ; ++idxPos){
       ++i;
       #ifdef PROGRESS
       CalcProgress(size, i);
       #endif
-      switch(rBuf[idxPos]){
-        case '>':  header = 1; continue;
-        case '\n': header = 0; continue;  
-        default:   if(header==1) continue;
+
+      switch(type){
+        case 1:
+        switch(rBuf[idxPos]){
+          case '>':  header = 1;
+          //TODO: RESET INDEX
+          // ResetIdx();
+          continue;
+          case '\n': header = 0; continue;  
+          default:   if(header==1) continue;
+          }
+        break;
+        case 2:
+          switch(line){
+            case 0: if(sym == '\n'){ line = 1; dna = 1; } break;
+            case 1: if(sym == '\n'){ line = 2; dna = 0; } break;
+            case 2: if(sym == '\n'){ line = 3; dna = 0; } break;
+            case 3: if(sym == '\n'){ line = 0; dna = 0; } break;
+            }
+        if(dna == 0 || sym == '\n') continue;
+
+        break;
         }
+        
       if((sym = S2N(rBuf[idxPos])) == 4) continue;    
       sBuf[idx] = sym;
       GetIdx(sBuf+idx-1, P->M);
+
       if(i > P->M->ctx){ // SKIP INITIAL CONTEXT, ALL "AAA..."
         Update(P->M);
         if(P->M->ir == 1){  // Inverted repeats
