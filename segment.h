12f5e21f2319f44696b9a8ee715ade7ead8da579
pratas <pratas@ua.pt>
2015-04-20 16:25:45 +0100

    segmentandpaint

---
+++ segment.h
@@ -0,0 +1,15 @@
+#ifndef SEGMENT_H_INCLUDED
+#define SEGMENT_H_INCLUDED
+
+#include "defs.h"
+
+#define LOW_REGION    0
+#define HIGH_REGION   1
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+void SegmentSequence(char *, Param *);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#endif
