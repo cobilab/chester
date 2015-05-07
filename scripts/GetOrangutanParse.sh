abff97f1f5f7468a8477487e061c36bc2e1dbc5d
pratas <pratas@ua.pt>
2015-05-07 15:42:12 +0100

    outputAsFasta

--- scripts/GetOrangutanParse.sh
+++ scripts/GetOrangutanParse.sh
@@ -31,41 +31,41 @@ for((x=1 ; x <= $MAX ; ++x));
     then
     ZPATH="$ONWAY$x.fa.gz";
     downloadEach "$WGETOP" "$ZPATH" "$x" "$INITALS";
-    zcat $INITALS-X$x | grep -v ">" | tr -d "\n" > $INITALS$x;
+    zcat $INITALS-X$x > $INITALS$x;
     echo "$INITALS C$x filtered!";
   fi
   done
 
 ZPATH=$ONWAY"2A.fa.gz";
 downloadEach "$WGETOP" "$ZPATH" "2A" "$INITALS";
-zcat $INITALS-X2A | grep -v ">" | tr -d "\n" > $INITALS"2A";
+zcat $INITALS-X2A > $INITALS"2A";
 echo "$INITALS C2A filtered";
 
 ZPATH=$ONWAY"2B.fa.gz";
 downloadEach "$WGETOP" "$ZPATH" "2B" "$INITALS";
-zcat $INITALS-X2B | grep -v ">" | tr -d "\n" > $INITALS"2B";
+zcat $INITALS-X2B > $INITALS"2B";
 echo "$INITALS C2B filtered";
 
 CHR=23;
 FIELD="X";
 ZPATH="$ONWAY$FIELD.fa.gz";
 downloadEach "$WGETOP" "$ZPATH" "$CHR" "$INITALS";
-zcat $INITALS-X$CHR | grep -v ">" | tr -d "\n" > $INITALS$CHR;
+zcat $INITALS-X$CHR > $INITALS$CHR;
 echo "$INITALS CX filtered";
 
 CHR=24;
 downloadEach "$WGETOP" "ftp://ftp.ncbi.nlm.nih.gov/genomes/Pongo_abelii/Assembled_chromosomes/seq/pab_ref_P_pygmaeus_2.0.2_unlocalized.fa.gz" "$CHR" "$INITALS";
-zcat $INITALS-X$CHR | grep -v ">" | tr -d "\n" > $INITALS$CHR;
+zcat $INITALS-X$CHR > $INITALS$CHR;
 echo "$INITALS UNLOCALIZED filtered";
 
 CHR=25;
 downloadEach "$WGETOP" "ftp://ftp.ncbi.nlm.nih.gov/genomes/Pongo_abelii/Assembled_chromosomes/seq/pab_ref_P_pygmaeus_2.0.2_unplaced.fa.gz" "$CHR" "$INITALS";
-zcat $INITALS-X$CHR | grep -v ">" | tr -d "\n" > $INITALS$CHR;
+zcat $INITALS-X$CHR > $INITALS$CHR;
 echo "$INITALS UNPLACED filtered";
 
 CHR=26;
 downloadEach "$WGETOP" "ftp://ftp.ncbi.nlm.nih.gov/genomes/Pongo_abelii/Assembled_chromosomes/seq/pab_ref_P_pygmaeus_2.0.2_chrMT.fa.gz" "$CHR" "$INITALS";
-zcat $INITALS-X$CHR | grep -v ">" | tr -d "\n" > $INITALS$CHR;
+zcat $INITALS-X$CHR > $INITALS$CHR;
 echo "$INITALS MITOCONDRIA filtered";
 
 rm -f *PA-* $INITALS-X2A $INITALS-X2B;
