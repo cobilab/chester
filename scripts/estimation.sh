21dfafbd91a299df6b914e8318398d2df7b21faf
pratas <pratas@ua.pt>
2016-11-11 16:00:22 +0000

    count

--- scripts/estimation.sh
+++ scripts/estimation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-N=`cat NEAN | grep -v ">" | wc -c`;
+#
+# usage: . estimation.sh <file.mfa> <k-mer>
+#
+K=`echo "scale=2;$2-1"  | bc -l`; #K-1
+N=`cat $1 | grep -v ">" | wc -c`;
+L=`cat $1 | grep -v ">" | wc -l`;
+echo "scale=2;$N-($L*$K)" | bc
+echo "$N";
+
+
+
 
