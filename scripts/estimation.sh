04224ccea9e2f064326373d17ca303de18c4d6c7
pratas <pratas@ua.pt>
2016-11-14 13:46:04 +0000

    estimation

--- scripts/estimation.sh
+++ scripts/estimation.sh
@@ -5,10 +5,7 @@
 K=`echo "scale=2;$2-1"  | bc -l`; #K-1
 C=`cat $1 | grep -v ">" | wc -c`;
 L=`cat $1 | grep -v ">" | wc -l`;
-N=`echo "scale=2;$C-($L*$K)" | bc`;
+N=`echo "scale=2;($C-($L*$K))*2" | bc`;
 #
-echo "N=$N";
-
-
-
+echo "N=$N"; #130942622992 * 2 // IR
 
