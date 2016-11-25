b7a1b41cc8f3566a826a347974947b2d2d304008
pratas <pratas@ua.pt>
2016-11-25 15:47:53 +0000

    description

--- src/chester-visual.c
+++ src/chester-visual.c
@@ -13,17 +13,6 @@
 #include "buffer.h"
 
 //////////////////////////////////////////////////////////////////////////////
-// - - - - - - - - - - - - - - - - W R I T E   W O R D - - - - - - - - - - - -
-void RWord(FILE *F, uint8_t *b, int32_t i, uint32_t kmer){
-  uint8_t w[kmer+1], n;
-  i -= kmer;
-  for(n = 0 ; n < kmer ; ++n)
-    w[n] = N2S(b[i+n]);
-  w[kmer] = '\0';
-  fprintf(F, "%s\n", w);
-  }
-
-//////////////////////////////////////////////////////////////////////////////
 // - - - - - - - - - - - - - - - - - P A I N T - - - - - - - - - - - - - - - -
 void PaintStreams(Param *P){
   FILE *Plot = Fopen("plot.svg", "w");
@@ -87,15 +76,14 @@ int32_t main(int argc, char *argv[]){
 
   if(ArgsState(DEFAULT_HELP, p, argc, "-h") == 1 || ArgsState(DEFAULT_HELP, p, 
   argc, "?") == 1 || argc < 3){
-    fprintf(stderr, "Usage: CHESTER-visual <OPTIONS>... [FILE]:<...> [FILE]:<...>\n");
-    fprintf(stderr, "CHESTER-visual: a tool to visualize relative singularity regions.\n");
+    fprintf(stderr, "Usage: CHESTER-visual <OPTIONS>... [FILE]:<...>\n");
+    fprintf(stderr, "CHESTER-visual: visualize relative singularity regions.\n");
     fprintf(stderr, "                                                     \n");
     fprintf(stderr, "  -v                       verbose mode,             \n");
     fprintf(stderr, "  -a                       about CHESTER,            \n");
     fprintf(stderr, "  -e <value>               enlarge painted regions,  \n");
     fprintf(stderr, "  -p                       show positions/words,     \n");
     fprintf(stderr, "                                                     \n");
-    fprintf(stderr, "  [rFile1]:<rFile2>:<...>  reference file(s),        \n");
     fprintf(stderr, "  [tFile1]:<tFile2>:<...>  target file(s).           \n");
     fprintf(stderr, "                                                     \n");
     fprintf(stderr, "Report bugs to <{pratas,ap,pjf}@ua.pt>.              \n");
@@ -103,23 +91,13 @@ int32_t main(int argc, char *argv[]){
     }
 
   P = (Param *) Calloc(1 , sizeof(Param));
-  P->ref       = ReadFNames (P, argv[argc-2]);  // REF
   P->tar       = ReadFNames (P, argv[argc-1]);  // TAR
-  P->kmer      = ArgsNum    (DEFAULT_KMER,    p, argc, "-k", MIN_KMER, MAX_KMER);
-  P->threshold = ArgsDouble (DEFAULT_THRESHOLD, p, argc, "-t");
-  P->subsamp   = ArgsNumI64 (DEFAULT_SAMPLE_RATIO, p, argc, "-u", -1, 999999);
-  P->window    = ArgsNumI64 (DEFAULT_WINDOW,  p, argc, "-w", -1,  9999999);
-  P->bSize     = ArgsNum64  (DEFAULT_BSIZE,   p, argc, "-s", 10, 2999999999999);
   P->enlarge   = ArgsNumI64 (DEFAULT_ENLARGE, p, argc, "-e", -1,  999999999);
   P->verbose   = ArgsState  (DEFAULT_VERBOSE, p, argc, "-v");
-  P->inverse   = ArgsState  (DEFAULT_IR,      p, argc, "-i");
-  P->disk      = ArgsState  (DEFAULT_DISK,    p, argc, "-p");
 
   if(P->verbose){
     fprintf(stderr, "==============[ CHESTER v%u.%u ]============\n",
     VERSION, RELEASE);
-    PrintArgs(P);
-    fprintf(stderr, "==========================================\n");
     }
 
   if(P->verbose) fprintf(stderr, "Painting ...\n");
@@ -130,7 +108,7 @@ int32_t main(int argc, char *argv[]){
     }
 
   if(P->verbose)
-    fprintf(stderr, "All jobs done in %.3g sec.\n", ((double)(clock()-start))/
+    fprintf(stderr, "Job done in %.3g sec.\n", ((double)(clock()-start))/
     CLOCKS_PER_SEC);
 
   return EXIT_SUCCESS;
