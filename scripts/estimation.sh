05a982efce229e952ac001ceb572851f7c1d494d
pratas <pratas@ua.pt>
2018-12-05 18:46:25 +0000

    update performance

--- scripts/estimation.sh
+++ scripts/estimation.sh
@@ -2,9 +2,9 @@
 #
 # usage: . estimation.sh <file.mfa> <k-mer>
 #
-K=`echo "scale=2;$2-1"  | bc -l`; #K-1
-C=`cat $1 | grep -v ">" | wc -c`;
-L=`cat $1 | grep -v ">" | wc -l`;
+K=`echo "scale=2;$2-1" | bc -l`; #K-1
+C=`grep -v ">" $1 | wc -c`;
+L=`grep -v ">" $1 | wc -l`;
 N=`echo "scale=2;($C-($L*$K))*2" | bc`;
 #
 echo "N=$N"; #130942622992 * 2 // IR
